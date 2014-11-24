4e25ab6501c553cab26e5cf6ac0fd24fd198caf9
Abhinav Rungta <abhinavrungta89@gmail.com>
2014-11-24 10:10:14 -0500

    misc

--- MyScalaProject/project4/src/Project4Server.scala
+++ MyScalaProject/project4/src/Project4Server.scala
@@ -2,8 +2,10 @@ package project4.src
 
 import java.util.concurrent.atomic.AtomicInteger
 
+import scala.collection.JavaConversions.asScalaBuffer
 import scala.collection.concurrent.TrieMap
 import scala.collection.mutable.ArrayBuffer
+import scala.collection.parallel.mutable.ParArray
 import scala.concurrent.duration.DurationInt
 
 import com.typesafe.config.ConfigFactory
@@ -24,13 +26,14 @@ object Project4Server {
     var timeStamp: Long = time
     //var mentions: MutableList[Int] = MutableList()
   }
-  var followersList = Array.fill(1)(ArrayBuffer.empty[ActorRef])
-  var followingList = Array.fill(1)(ArrayBuffer.empty[ActorRef])
+  var followersList = Array.fill(1)(ParArray.empty[Int])
+  var followingList = Array.fill(1)(ParArray.empty[Int])
   var tweetTPS = ArrayBuffer.empty[Int]
   var ctr: AtomicInteger = new AtomicInteger()
   var rdctr: AtomicInteger = new AtomicInteger()
+
   var tweetStore = new TrieMap[Int, Tweets]
-  var timeLines = new TrieMap[Int, ArrayBuffer[Int]]
+  var timeLines = Array.fill(1)(java.util.Collections.synchronizedList(new java.util.ArrayList[Int]()))
 
   def main(args: Array[String]) {
     // create an actor system.
@@ -54,7 +57,7 @@ object Project4Server {
     var cancellable = system.scheduler.schedule(0 seconds, 5000 milliseconds, self, Time)
 
     // Start a router with 30 Actors in the Server.
-    val router = context.actorOf(Props[Server].withRouter(SmallestMailboxRouter(10)), name = "Router")
+    val router = context.actorOf(Props[Server].withRouter(SmallestMailboxRouter(6)), name = "Router")
     // Watch the router. It calls the terminate sequence when router is terminated.
     context.watch(router)
 
@@ -63,15 +66,14 @@ object Project4Server {
       var noOfUsers = nodesArr.length
       // create a distribution for followers per user.
       var FollowersPerUser = pdf.exponential(1.0 / 208.0).sample(noOfUsers).map(_.toInt)
-      FollowersPerUser = FollowersPerUser.sortBy(a => a)
-
+      FollowersPerUser = FollowersPerUser.sortBy(a => -a)
       // create a distribution for followings per user.
-      var FollowingPerUser = pdf.exponential(1.0 / 208.0).sample(noOfUsers).map(_.toInt)
+      var FollowingPerUser = pdf.exponential(1.0 / 238.0).sample(noOfUsers).map(_.toInt)
       FollowingPerUser = FollowingPerUser.sortBy(a => a)
 
       // create a list for n users.
-      followersList = Array.fill(noOfUsers)(ArrayBuffer.empty[ActorRef])
-      followingList = Array.fill(noOfUsers)(ArrayBuffer.empty[ActorRef])
+      var followers = Array.fill(noOfUsers)(ArrayBuffer.empty[ActorRef])
+      var following = Array.fill(noOfUsers)(ArrayBuffer.empty[ActorRef])
 
       // assign followers to each user.
       for (j <- 0 to noOfUsers - 1) {
@@ -79,12 +81,9 @@ object Project4Server {
         // construct list of followers.
         while (FollowingPerUser(j) > 0 && k < noOfUsers) {
           k += 1
-          while (k < noOfUsers && FollowersPerUser(k) > 0) {
-            k += 1
-          }
-          if (k < noOfUsers) {
-            followingList(j) += nodesArr(k)
-            followersList(k) += nodesArr(j)
+          if (k < noOfUsers && FollowersPerUser(k) > 0) {
+            following(j) += nodesArr(k)
+            followers(k) += nodesArr(j)
             FollowingPerUser(j) -= 1
             FollowersPerUser(k) -= 1
           }
@@ -93,15 +92,19 @@ object Project4Server {
 
       // send followers and following list to each user.
       for (j <- 0 to noOfUsers - 1) {
-        nodesArr(j) ! Project4Client.Client.FollowingList(followingList(j))
-        nodesArr(j) ! Project4Client.Client.FollowersList(followersList(j))
+        nodesArr(j) ! Project4Client.Client.FollowingList(following(j))
+        nodesArr(j) ! Project4Client.Client.FollowersList(followers(j))
+      }
+
+      followersList = Array.fill(noOfUsers)(scala.collection.parallel.mutable.ParArray.empty[Int])
+      followingList = Array.fill(noOfUsers)(scala.collection.parallel.mutable.ParArray.empty[Int])
+      for (j <- 0 to noOfUsers - 1) {
+        followersList(j) = followers(j).map(a => a.path.name.toInt).toArray.par
+        followingList(j) = following(j).map(a => a.path.name.toInt).toArray.par
       }
 
       // initialize timelines data.
-      for (j <- 0 to noOfUsers - 1) {
-        timeLines += (j -> ArrayBuffer())
-      }
-
+      timeLines = Array.fill(noOfUsers)(java.util.Collections.synchronizedList(new java.util.ArrayList[Int]()))
       println("Server started")
     }
 
@@ -121,7 +124,7 @@ object Project4Server {
           println(rdctr.get())
           println(tweetStore.size)
           for (j <- 0 to followersList.size - 1) {
-            println(timeLines(j).size)
+            //            println(timeLines(j).size)
           }
           system.shutdown
         }
@@ -146,10 +149,7 @@ object Project4Server {
         tweetStore += (tweetId -> new Tweets(userId, msg, time)) // create object for the tweet store and add.
 
         var followers = followersList(userId) // get all followers and add tweet to their timeline
-        for (j <- 0 to followers.size - 1) {
-          var index = followers(j).path.name.toInt
-          timeLines(index) += tweetId
-        }
+        followers.foreach(a => { timeLines(a) += tweetId })
         timeLines(userId) += tweetId // add to self timeline also
 
       case SendTimeline(userId) =>
